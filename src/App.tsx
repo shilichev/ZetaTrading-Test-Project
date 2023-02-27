8cef76c8b67aab7de7991f81def1544b58e038a5
Daniil <Shylichau>
2023-02-27 17:49:33 +0100

    added layout ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-
 import "./App.css";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material";
+import { Layout } from "./common/Layout";
 
 function App() {
-  return <div className="App"></div>;
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#2E7D32",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Layout>1</Layout>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/common/Layout.tsx
@@ -0,0 +1,96 @@
+import { ArrowBack, Menu } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Button,
+  CssBaseline,
+  Drawer,
+  IconButton,
+  Toolbar,
+} from "@mui/material";
+import { ReactNode, useState } from "react";
+import { SideBar } from "./SideBar";
+
+const drawerWidth = 240;
+
+interface ILayoutProps {
+  children: ReactNode;
+}
+
+export const Layout = ({ children }: ILayoutProps) => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { lg: `calc(100% - ${drawerWidth}px)` },
+          ml: { lg: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { lg: "none" } }}
+          >
+            <Menu />
+          </IconButton>
+          <Button color="inherit" aria-label="open drawer" onClick={() => {}}>
+            <ArrowBack />
+            Back
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { lg: drawerWidth }, flexShrink: { lg: 10 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          anchor="top"
+          variant="temporary"
+        >
+          {<SideBar />}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { lg: "block", md: "none", xs: "none", sl: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {<SideBar />}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { lg: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
+  );
+};
